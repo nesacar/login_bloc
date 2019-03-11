eaa882ad37031701a57dd9ba283267fc7771047a
nebojsa <nebojsart1409@yahoo.com>
2019-03-11 21:01:27 +0100

    Advanced Flutter Architecture - The Bloc Pattern

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,6 +1,19 @@
 import 'dart:async';
 
-class Bloc {
-  final emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
+import 'validators.dart';
+
+class Bloc extends Object with Validators {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose(){
+    _email.close();
+    _password.close();
+  }
 }
